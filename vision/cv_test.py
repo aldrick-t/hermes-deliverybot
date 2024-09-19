64ca7eff0609e96e29ae5894c9ca465d32ac37f6
Aldrick T <taldrick@gmail.com>
2024-09-19 11:55:45 -0600

    added edge detect with opencv Canny

--- vision/cv_test.py
+++ vision/cv_test.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-cap = cv2. VideoCapture (0)
+cap = cv2. VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
@@ -12,17 +13,30 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     #Color range selected to pale lime for testing
-    lower_lime = np.array([30,20,20])
-    upper_lime = np.array([100,255,255])
+    # https://colorizer.org/ 
+    lower_limit = np.array([78,20,20]) #blue range
+    upper_limit = np.array([260,255,255])
     
     #mask to select the color range in the frame
-    mask = cv2.inRange(hsv, lower_lime, upper_lime)
+    mask = cv2.inRange(hsv, lower_limit, upper_limit)
     
     #comprarator result, to show only the color range in the frame
     #blends image with bitwise_and then selects using mask
     result = cv2.bitwise_and(frame, frame, mask=mask)
     
+    #laplasian filter
+    laplasian = cv2.Laplacian(mask, cv2.CV_64F)
+    laplasian = np.uint8(laplasian)
+    cv2.imshow('laplasian', laplasian)
+    
+    #canny edge detection
+    edges = cv2.Canny(mask, 100, 100)
+    cv2.imshow('Canny', edges)
+    
+    #display the result frame and mask
     cv2.imshow('frame', result)
+    cv2.imshow('mask', mask)
+    
     if cv2.waitKey(1) == ord('q'):
         break
 
